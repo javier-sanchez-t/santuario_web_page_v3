6a731a51a662b8def847f3806e424eaa95853514
Javier Sánchez T <jefe10jav@gmail.com>
2020-05-23 19:51:38 -0500

    JST - Footer added to index

--- js/custom.js
+++ js/custom.js
@@ -10,6 +10,31 @@ function goToSection(sectionName) {
  }, 1000);
 }
 
+// Restricts input for the given textbox to the given inputFilter.
+function setInputFilter(textbox, inputFilter) {
+    ["input", "keydown", "keyup", "mousedown", "mouseup", "select", "contextmenu", "drop"].forEach(function (event) {
+        textbox.addEventListener(event, function () {
+            if (inputFilter(this.value)) {
+                this.oldValue = this.value;
+                this.oldSelectionStart = this.selectionStart;
+                this.oldSelectionEnd = this.selectionEnd;
+            } else if (this.hasOwnProperty("oldValue")) {
+                this.value = this.oldValue;
+                this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
+            }
+        });
+    });
+}
+
+// Install input filters.
+setInputFilter(document.getElementById("cantidad"), function (value) {
+    return /^-?\d*$/.test(value);
+});
+
+setInputFilter(document.getElementById("noReservacion"), function (value) {
+    return /^-?\d*$/.test(value);
+});
+
 function clearModalFacturacion() {
  document.getElementById("noReservacion").value = '';
  document.getElementById("fechaReservacion").value = '';
